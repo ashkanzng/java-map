ab0c8f65ab6d5d8b2e9c9d934e9622ddb3ef64fb
Ashkan <ash333d@gmail.com>
2021-02-28 15:57:26 +0330

    Updating db

--- src/test/java/com/devlon/services/CompanyServiceTest.java
+++ src/test/java/com/devlon/services/CompanyServiceTest.java
@@ -1,34 +1,60 @@
 package com.devlon.services;
 
+import com.devlon.app.DevlonApplication;
 import com.devlon.models.Company;
 import com.devlon.repositories.CompanyRepository;
+import com.devlon.transactionObjects.CompanyT;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-
-@SpringBootTest(classes = {CompanyRepository.class})
-@AutoConfigureMockMvc
+@SpringBootTest(classes = {DevlonApplication.class})
 class CompanyServiceTest {
 
+    @Autowired
+    CompanyService companyService;
 
-    @MockBean
-    CompanyRepository companyRepository;
+    @PersistenceContext
+    EntityManager entityManager;
 
+    // Test company with children
     @Test
+    @Transactional
     void get() {
 
-        Optional<Company> c = companyRepository.findById(1);
+        Company company = new Company();
+        company.setName("Parent");
+        company.setStations(new HashSet<>());
+        Company parentCompany = companyService.create(company);
 
-        if (c.isPresent()){
-            System.out.println(c.get());
-        }else {
-            System.out.println("empty");
 
-        }
+        Company child_company = new Company();
+        child_company.setName("Child 1");
+        child_company.setStations(new HashSet<>());
+        child_company.setParentId(company.getId());
+        companyService.create(child_company);
+
+        Company child_2_company = new Company();
+        child_2_company.setName("Child 2");
+        child_2_company.setStations(new HashSet<>());
+        child_2_company.setParentId(company.getId());
+        companyService.create(child_2_company);
+
+
+        CompanyT tree = companyService.get(parentCompany.getId());
+        System.out.println(tree.getChild().size());
+        assertEquals(tree.getChild().size(),2);
 
     }
 }
